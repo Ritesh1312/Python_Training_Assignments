f00d24152e48483119d32bcde68230baf8be9e16
Ritesh <riteshrk1312@gmail.com>
2023-10-04 01:12:25 +0530

    Modification done

---
+++ Assignment_7/app.py
@@ -0,0 +1,20 @@
+from banking.bank import Bank
+from banking.atm import ATM
+def main():
+    print('HELLO (^_^)')
+    sbi = Bank('SBI', 12)
+    atm = ATM(sbi)
+    sbi.create_account('Ritesh' ,'pass', 50000)
+    sbi.create_account('Mohak' ,'pass', 50000)
+    sbi.create_account('Pradumn' ,'pass', 50000)
+    sbi.create_account('Amitabh' ,'pass', 50000)
+    sbi.create_account('Aditya1' ,'pass', 50000)
+    sbi.info_all__accounts()
+    while True:
+        if atm.start():
+            continue
+
+
+
+if __name__=='__main__':
+    main()
