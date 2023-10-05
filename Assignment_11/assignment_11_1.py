637bb3dbbc7e9b6837dcab2f96bda66e0d324f18
Ritesh <riteshrk1312@gmail.com>
2023-10-06 00:30:12 +0530

    Assignment 11 added

---
+++ Assignment_11/assignment_11_1.py
@@ -0,0 +1,37 @@
+import time
+import math
+
+
+def decorator(func):
+    def inner(*args, **kwargs):
+        start = time.time()
+        returned_value = func(*args, **kwargs)
+        end = time.time()
+        print(f'Time taken to execute = {end - start}')
+        return returned_value
+     
+    return inner
+    
+@decorator
+def find_primes(min,max):
+    primes=[]
+    start = time.time()
+    for x in range(min,max):
+        if is_prime(x):
+            primes.append(x)
+    end = time.time()
+    time_taken = end - start
+    return primes
+
+def is_prime(n):
+    if n<2:
+        return False
+    for x in range(2,int(math.sqrt(n))+1):
+        if n%x==0:
+            return False
+    return True
+
+
+x=find_primes(1,100000)
+
+print(len(x))   
---
+++ Assignment_11/assignment_11_2.py
@@ -0,0 +1,35 @@
+import time
+import math
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
+        return self
+    
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.time_taken = self.end - self.start
+
+
+def find_primes(min,max):
+    primes=[]
+    start = time.time()
+    for x in range(min,max):
+        if is_prime(x):
+            primes.append(x)
+    end = time.time()
+    time_taken = end - start
+    return primes
+
+def is_prime(n):
+    if n<2:
+        return False
+    for x in range(2,int(math.sqrt(n))+1):
+        if n%x==0:
+            return False
+    return True
+
+with Timer() as t:
+    prime = find_primes(2,500000)
+    print(f'Total Primes are - {len(prime)}')
+
+print(f'Total Time Taken - {t.time_taken}')
